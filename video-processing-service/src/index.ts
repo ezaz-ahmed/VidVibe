5877a711b2033d97d288bc08b2e99f531aa5374a
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-06-03 14:34:05 +0600

    🪶 video processing api route is working

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,14 +1,40 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("It's working...");
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    return res.status(400).send("Bad Request: Missing file path");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      res.status(200).send("Video processing finished successfully");
+    })
+    .on("error", (err) => {
+      console.error(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(
     `Video processing service is listening at http://localhost:${port}`
   );
+
+  ffmpeg.getAvailableFormats((err, formats) => {
+    if (err) {
+      console.error("FFmpeg error:", err.message);
+    } else {
+      console.log("FFmpeg is available and working");
+    }
+  });
 });
